9c304366e351cbae08a14bd469620315da4f490a
Mauricio Diaz <m.diazt@uniandes.edu.co>
2019-04-05 21:49:23 -0500

    llave de sesion enviada

--- src/caso2/ProtocoloCliente.java
+++ src/caso2/ProtocoloCliente.java
@@ -13,6 +13,7 @@ import javax.xml.bind.DatatypeConverter;
 
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.X509Extension;
+import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
@@ -25,43 +26,54 @@ import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 public class ProtocoloCliente {
 
 	public static void procesar(BufferedReader stdIn, BufferedReader pIn,PrintWriter pOut) throws Exception {
-				
-		
-		System.out.println("Escriba el mensaje para Enviar:");
-		String fromUser = stdIn.readLine();
-		
+
 		pOut.println("HOLA");
-		
+
 		String fromServer="";
-		
+
 		if((fromServer = pIn.readLine())!= null) {
 			System.out.println("Respuesta del Servidor: " + fromServer );
 		}
-		
+
 		pOut.println("ALGORITMOS:AES:RSA:HMACSHA1");
-		
+
 		if((fromServer = pIn.readLine())!= null) {
 			System.out.println("Respuesta del Servidor: " + fromServer );
 		}
-		
+
 		KeyPair keyClient = KeyPairGenerator.getInstance("RSA", new BouncyCastleProvider()).generateKeyPair();
 		pOut.println(generarCertificado(keyClient));
+
+		if((fromServer = pIn.readLine())!= null) {
+			System.out.println("Respuesta del Servidor: " + fromServer );
+		}
+		
+		PublicKey llaveServidor = leerCertificado(fromServer).getPublicKey();
+
+
+		byte[] arr = new byte[32];
+		SecretKey llaveSesion = new SecretKeySpec(arr,"AES");
+		
+		String llaveCifrada= DatatypeConverter.printHexBinary(cifradoAsimetrico(llaveServidor,llaveSesion.getEncoded()));
+		pOut.println(llaveCifrada);
 		
 		if((fromServer = pIn.readLine())!= null) {
 			System.out.println("Respuesta del Servidor: " + fromServer );
 		}
 		
-		byte[] arr = new byte[32];
-		SecretKey llaveS = new SecretKeySpec(arr,"AES");
-		//String llaveCifrada= DatatypeConverter.printHexBinary(cifrarAsimetrico(llaveS.getEncoded()));
+		
+		
+		pOut.println("OK");
+		
+		
 	}
-	
+
 	public static String generarCertificado(KeyPair pair) throws Exception
 	{
-	
+
 		PublicKey pub = pair.getPublic();
 		PrivateKey priv = pair.getPrivate();
-		
+
 		JcaX509ExtensionUtils extUtils = new JcaX509ExtensionUtils();
 		X509v3CertificateBuilder v3CertGen = new JcaX509v3CertificateBuilder(
 				new X500Name("CN=0.0.0.0, OU=None, O=None, L=None, C=None"), 
@@ -71,16 +83,29 @@ public class ProtocoloCliente {
 				new X500Name("CN=0.0.0.0, OU=None, O=None, L=None, C=None"), pub);
 		v3CertGen.addExtension(X509Extension.subjectKeyIdentifier, false, extUtils.createSubjectKeyIdentifier(pub));
 		v3CertGen.addExtension(X509Extension.authorityKeyIdentifier, false,	extUtils.createAuthorityKeyIdentifier(pub));
-		
 
-	X509Certificate certificado = new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider()).getCertificate(v3CertGen.build(new JcaContentSignerBuilder("MD5withRSA").setProvider(new BouncyCastleProvider()).build(priv)));
-		
-	byte [] certificadoEnBytes = certificado.getEncoded();		
-	
-	String respuesta= DatatypeConverter.printHexBinary(certificadoEnBytes);
+
+		X509Certificate certificado = new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider()).getCertificate(v3CertGen.build(new JcaContentSignerBuilder("MD5withRSA").setProvider(new BouncyCastleProvider()).build(priv)));
+
+		byte [] certificadoEnBytes = certificado.getEncoded();		
+
+		String respuesta= DatatypeConverter.printHexBinary(certificadoEnBytes);
 		return respuesta;
-		
-		
+
+
+	}
+
+	public static X509Certificate leerCertificado(String str) throws Exception
+	{
+		byte[] certificadoEnBytes= DatatypeConverter.parseHexBinary(str);	
+		X509Certificate cert = new JcaX509CertificateConverter().getCertificate(new X509CertificateHolder(certificadoEnBytes));
+		return cert;
 	}
 	
+	public static byte[] cifradoAsimetrico(PublicKey llave, byte[] msg) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, llave);
+		return cipher.doFinal(msg);
+	}
+
 }
